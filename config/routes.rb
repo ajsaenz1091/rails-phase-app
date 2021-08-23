ab0a314a1e0b4e5fcad05e2f6c2671e5fb3f386e
Alberto <ajsaenz1091@gmail.com>
2021-08-23 14:43:03 -0400

    added .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :comments
-  resources :users
+  namespace :api do
+    resources :posts
+    resources :comments
+    resources :users
 
-  get '/me', to: 'users#show'
+    get '/me', to: 'users#show'
 
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
