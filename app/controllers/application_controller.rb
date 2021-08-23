c8051388a0d27a7eadf90299a81ee268c4251b64
Alberto <ajsaenz1091@gmail.com>
2021-08-23 19:44:36 -0400

    Added Authorize method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::API
     def render_unprocessable_entity_response(invalid)
         render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
+
+    def authorize
+        if !session[:user_id]
+            render json: { errors: ["Not Authorized"] }, status: :unauthorized
+        else
+            @current_user = User.find(session[:user_id])
+        end 
+    end
 end
