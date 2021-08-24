c7433946302be84cd090f9579b90850175231b29
Alberto <ajsaenz1091@gmail.com>
2021-08-23 21:29:08 -0400

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
     delete '/logout', to: 'sessions#destroy'
   end
 
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "/me", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
